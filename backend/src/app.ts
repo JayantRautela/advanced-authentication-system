460e1dd973e913bb243c78df4d8c995818609703
Jayant Singh Rautela <jayantrautela0205@gmail.com>
2025-04-02 20:33:02 +0530

    add Prisma client  and initialize User model migration

---
+++ backend/src/app.ts
@@ -0,0 +1,9 @@
+import express, { Express } from "express";
+
+const app: Express = express();
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+    console.log(`Server started at port ${port}`);
+});
