f40d78c49a421e48f02e89e7813c71697c2ff3b2
Jayant Singh Rautela <jayantrautela0205@gmail.com>
2025-04-06 18:50:20 +0530

    setting up form

--- frontend/src/pages/Signup.tsx
+++ frontend/src/pages/Signup.tsx
@@ -1,5 +1,13 @@
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useState } from "react"
+import { useForm } from "react-hook-form"
+import * as z  from "zod"
 
 const Signup = () => {
+  const [username, setUsername] = useState('');
+  const [usernameMessage, setUsernameMessage] = useState('');
+  const [isCheckingUsername, setIsCheckingUsername] = useState(false);
+  const [isSubmitting, setIsSubmitting] = useState(false);
   return (
     <div>
       Signup
