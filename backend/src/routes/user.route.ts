7c8a691b0413c22ab41fc4ad9a1ca0640c1778d9
Jayant Singh Rautela <jayantrautela0205@gmail.com>
2025-04-06 22:15:06 +0530

    add authentication middleware

--- backend/src/routes/user.route.ts
+++ backend/src/routes/user.route.ts
@@ -1,11 +1,12 @@
 import express from "express";
 import { login, logout, register, sendOtp, verifyOtp } from "../controllers/user.controller";
+import { isAuthenticated } from "../middleware/isAuthenticated";
 
 const router = express.Router();
 
 router.post('/register', register as unknown as  express.RequestHandler);
 router.post('/login', login as unknown as express.RequestHandler);
-router.post('/logout', logout as unknown as express.RequestHandler);
+router.post('/logout', isAuthenticated as unknown as express.RequestHandler, logout as unknown as express.RequestHandler);
 router.post('/sendOtp', sendOtp as unknown as express.RequestHandler);
 router.post('/verifyOtp', verifyOtp as unknown as express.RequestHandler);
 
