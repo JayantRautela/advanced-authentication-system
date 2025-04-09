1f60f38f76dc5d176a3cd7f89f16eefe136688ce
Jayant Singh Rautela <jayantrautela0205@gmail.com>
2025-04-09 21:34:56 +0530

    add cors middleware

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,16 @@
 import express, { Express } from "express";
 import UserRouter from "./routes/user.route";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 import dotenv from "dotenv";
 dotenv.config();
 
 const app: Express = express();
+
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true
+}))
 
 app.use(express.json());
 app.use(cookieParser());
